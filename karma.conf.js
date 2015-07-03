5a1875b962855cff2f1a582404ad3f863b413d59
heucles <heucles@yahoo.com.br>
2015-07-03 18:50:41 -0300

    Testing Travis CI: Trying to fix the build XIII

--- karma.conf.js
+++ karma.conf.js
@@ -77,4 +77,23 @@ module.exports = function (config) {
         // if true, it capture browsers, run tests and exit
         singleRun: false
     });
+	
+	
+	var configuration = {
+    // other things
+ 
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+};
+ 
+if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+}
+ 
+config.set(configuration);
+
 };
